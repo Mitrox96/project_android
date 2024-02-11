95bf09db9fd13c352de1c9023a12ea8cac111fae
DORIANOV <dorian.groner33@gmail.com>
2024-02-11 23:15:06 +0100

    details

--- app/src/main/java/fr/ugatir/cda1_android/DetailsFilm.java
+++ app/src/main/java/fr/ugatir/cda1_android/DetailsFilm.java
@@ -1,4 +1,23 @@
-package fr.ugatir.cda1_android;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Detailsfilm {
+public class DetailsFilm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnPanier = findViewById(R.id.btnPanier);
+
+
+        btnPanier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
 }
